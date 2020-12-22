40527a7e8ff1685ccca93c780b73222714a8eeaf
Sebastian Asis <sasisromero10@gmail.com>
2020-12-22 18:19:49 -0500

    Configuraciones de handlebars

--- Desarrollo/SGEH/Despliegue/SGEH-WEB/src/index.js
+++ Desarrollo/SGEH/Despliegue/SGEH-WEB/src/index.js
@@ -9,20 +9,20 @@ const app = express();
 
 // Settings
 app.set('port', process.env.PORT || 4000);
-// app.set('views', path.join(__dirname, 'views'));
-// app.engine('.hbs', exphbs({
-//     defaultLayout: 'main',
-//     layoutsDir: path.join(app.get('views'), 'layouts'),
-//     partialsDir: path.join(app.get('views'), 'partials'),
-//     extname: '.hbs',
-//     helpers: require('./lib/handlebars')
-// }))
-// app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars')
+}))
+app.set('view engine', '.hbs');
 
 // Middlewares
 
 app.use(morgan('dev'));
-// app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: false}));
 
 //Routes
 app.use(require('./routes/index.js'));
